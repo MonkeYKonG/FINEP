18608e32b1b9fdbbf712f9300d29fd522a2cb6f8
Alexis CRAYE <alexis1.craye@epitech.eu>
2018-04-07 11:40:02 +0200

    correct routes + Ggemfile

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -8,7 +8,7 @@ Rails.application.routes.draw do
   post 'users/signup' => 'users#sign_up'
   post 'users/signin' => 'users#sign_in'
   post 'users/forgot' => 'users#forgot_passwd'
-  update 'users/update' => 'users#update_profile'
+  patch 'users/update' => 'users#update_profile'
 
   # Contracts routes
   get 'contracts' => 'contracts#get_contracts'
