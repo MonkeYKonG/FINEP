a9e3f5a40976f59d8c4ac2ac3142ba719691c9ef
Alexis CRAYE <alexis1.craye@epitech.eu>
2018-04-07 16:05:16 +0200

    copy retractablepanel from the poc

--- frontend/src/app/components/retractable-panel/retractable-panel.component.ts
+++ frontend/src/app/components/retractable-panel/retractable-panel.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-retractable-panel',
@@ -7,7 +7,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RetractablePanelComponent implements OnInit {
 
-  constructor() { }
+  @Input('isOpen') isOpen;
+  @Input('title') title;
+  @Output() openClick = new EventEmitter<any>();
+
+  buttonClicked() {
+    this.openClick.emit();
+  }
+
+  constructor() {
+    this.isOpen = true;
+  }
 
   ngOnInit() {
   }
