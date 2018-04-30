1fbf1388920b7400934a58bf7a579dd9b86e1277
Unknown <xavier.loire@epitech.eu>
2018-04-30 18:42:35 +0200

    Task 21
    Manque les tests unitaires

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,6 +1,25 @@
 class UsersController < ApplicationController
     def sign_up
-        @new_user = User.create email: params[:email], firstname: params[:firstname], name: params[:name], pseudo: params[:pseudo], capital: 0, passwd: params[:passwd]
+        @new_user = User.new
+        if params[:Action] == "signup" && params[:email] != "" && params[:firstname] != "" && params[:name] != "" && params[:pseudo] != "" && params[:passwd] != ""
+            @new_user.email = params[:email]
+            @new_user.firstname = params[:firstname]
+            @new_user.name = params[:name]
+            @new_user.pseudo = params[:pseudo]
+            @new_user.capital = 0
+            @new_user.passwd = params[:passwd]
+            if !User.exists?(:pseudo => @new_user.pseudo) && !User.exists?(:email => @new_user.email)
+                if @new_user.save
+                    render :status => 201 and return
+                else
+                    render :status => 500 and return
+                end
+            else
+                render :status => 403 and return
+            end
+        else
+            render :status => 404 and return
+        end
         render json: @new_user
     end
 
