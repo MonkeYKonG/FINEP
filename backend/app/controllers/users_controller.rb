806e25ba31025b10383e6f40830e3497cdc04984
Unknown <xavier.loire@epitech.eu>
2018-05-01 19:32:57 +0200

    Start Task 24
    Manque les test unitaires, la génération de mdp et la modification de db

--- backend/app/controllers/users_controller.rb
+++ backend/app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
     def sign_up
         @new_user = User.new
-        if params[:Action] == "signup" && params[:email] != "" && params[:firstname] != "" && params[:name] != "" && params[:pseudo] != "" && params[:passwd] != ""
+        if params[:action] == "signup" && params[:email] != "" && params[:firstname] != "" && params[:name] != "" && params[:pseudo] != "" && params[:passwd] != ""
             @new_user.email = params[:email]
             @new_user.firstname = params[:firstname]
             @new_user.name = params[:name]
@@ -24,7 +24,7 @@ class UsersController < ApplicationController
     end
 
     def sign_in
-        if params[:Action] && params[:Action] == "signin"
+        if params[:action] && params[:action] == "signin"
             if params[:email] != "" && params[:passwd] != ""
                 @user = User.where(pseudo: params[:email], passwd: params[:passwd])
                 if @user && @user.size == 1
@@ -44,7 +44,23 @@ class UsersController < ApplicationController
     end
 
     def forgot_passwd
-
+        if params[:action] && params[:action] == "lostpasswd"
+            if params[:email] != ""
+                bool = User.exists?(:email => params[:email])
+                if bool
+                    render :status => 201 and return
+                    // envoyer un mail avec un nouveau mdp aléatoire après avoir modifier la db
+                elsif !bool
+                    render :status => 400 and return
+                else
+                    render :status => 500 and return
+                end
+            else
+                render :status => 400
+            end
+        else
+            render :status => 404 and return
+        end
     end
 
     def get_profiles
