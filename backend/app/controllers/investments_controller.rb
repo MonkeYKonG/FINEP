5e03ccf07ec003b17c1830e380ac9922d86b16c5
Unknown <xavier.loire@epitech.eu>
2018-09-28 21:13:30 +0800

    Task 28
    Début de l'implémentation de l’investissement côté back-end

--- backend/app/controllers/investments_controller.rb
+++ backend/app/controllers/investments_controller.rb
@@ -2,6 +2,28 @@ class InvestmentsController < ApplicationController
     def invest
         @new_investment = Investment.create authorid: params[:author_id], destid: params[:dest_id], value: params[:value]
         render json: @new_investment
+        if params[:Action] == "signcontrat"
+            if params[:id] && params[:id] != "" && params[:targetid] && params[:targetid] != "" && params[:invest] && params[:invest] != ""
+                u = User.exists?(:id => params[:id])
+                t = User.exists?(:id => params[:targetid])
+                if u && t
+                    target = User.find(:params[:targetid])
+                    user = User.find(:params[:id])
+                    /* Attention !!! Je n'ai pas encore ajouter la colone open à la table user*/
+                    if target.open && user.capital >= Integer(params[:invest])
+                        render :status => 201 and return
+                    else
+                        render :status => 400 and return
+                    end
+                else
+                    render :status => 400 and return
+                end
+            else
+                render :status => 400 and return
+            end
+        else
+          render :status => 404
+        end
     end
 
     def get_investments
